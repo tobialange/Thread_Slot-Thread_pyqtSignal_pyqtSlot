f2d4938ca775888bad622d91e9159ee5e80e9755
tobialange <134139908+tobialange@users.noreply.github.com>
2023-05-21 12:01:57 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from PyQt6.QtWidgets import QApplication
+import sys
 from MyPrinter import MyPrinter
 from MyThread import MyThread
 
@@ -7,3 +9,7 @@ printer.print_my_text("Hallo Welt")
 thread = MyThread()
 thread.text_to_print.connect(printer.print_my_text)
 thread.start()
+
+
+app = QApplication(sys.argv)
+sys.exit(app.exec())
